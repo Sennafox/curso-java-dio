30c9a6ff9dc5e6236dd16abe48f6f9b1a1f63b44
Paulo Senna <65934431+Sennafox@users.noreply.github.com>
2022-02-24 22:45:25 -0400

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+import java.sql.SQLOutput;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+        
+        /*int a, b;
+        a = 4;
+        b = 6;
+        System.out.println(a + " + " + b + " é igual a " + (a+b));*/
+    }
+}
