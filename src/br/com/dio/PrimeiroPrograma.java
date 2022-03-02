a98159dffd0d4f3875fbc6fed2f20a19bfca029b
Paulo Senna <65934431+Sennafox@users.noreply.github.com>
2022-03-01 20:34:51 -0400

    Criando classe livro1

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,13 +6,44 @@ import java.sql.SQLOutput;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro1 = new Livro();
+        System.out.println(livro1);
         
         /*int a, b;
         a = 4;
         b = 6;
         System.out.println(a + " + " + b + " é igual a " + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
